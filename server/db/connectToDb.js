c532650bcbb87ca69e0ad45b29167581f1dacd97
ffastym <yuriy.matviyuk22@gmail.com>
2019-07-24 00:14:11 +0300

    add production db

--- server/db/connectToDb.js
+++ server/db/connectToDb.js
@@ -1,19 +1,21 @@
 /**
  * @author Yuriy Matviyuk
  */
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const dbRoute = "mongodb://localhost:27017/battle";
+const dbRoute = process.env.NODE_ENV === 'production'
+  ? 'mongodb://ffastym:Tt239allo@ds353957.mlab.com:53957/heroku_rpxcq589'
+  : 'mongodb://localhost:27017/battle'
 
-mongoose.connect(dbRoute, { useNewUrlParser: true });
+mongoose.connect(dbRoute, { useNewUrlParser: true })
 
 const connectToDb = () => {
-    const db = mongoose.connection;
+  const db = mongoose.connection
 
-    db.once("open", () => console.log("connected to the database"));
+  db.once('open', () => console.log('connected to the database'))
 
-    // checks if connection with the database is successful
-    db.on("error", console.error.bind(console, "MongoDB connection error:"));
-};
+  // checks if connection with the database is successful
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+}
 
-export default connectToDb;
+export default connectToDb
